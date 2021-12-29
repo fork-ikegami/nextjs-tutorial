19db824d86eadadeedc5d4fe4d70b81d28948e1b
Maki Ikegami <76510906+fork-ikegami@users.noreply.github.com>
2021-12-29 18:12:36 +0900

    Update layout.js

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
@@ -28,8 +28,7 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
+            <img
               src="/images/profile.png"
               className={utilStyles.borderCircle}
               height={144}
@@ -42,8 +41,7 @@ export default function Layout({ children, home }) {
           <>
             <Link href="/">
               <a>
-                <Image
-                  priority
+                <img
                   src="/images/profile.png"
                   className={utilStyles.borderCircle}
                   height={108}
